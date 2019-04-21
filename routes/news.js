ccb231374dc745810a1c0e98efdef3ae07b40959
Bianca <ing_biank@hotmail.com>
2019-04-21 22:55:29 +0200

    Adds HackerNews as data resource

--- routes/news.js
+++ routes/news.js
@@ -23,6 +23,28 @@ const scrapeMedium = async () => {
   return scrapedData
 }
 
+const scrapeHackerNews = async () => {
+  const browser = await puppeteer.launch()
+  const page    = await browser.newPage()
+  const query   = '?query=JavaScript&sort=byPopularity&dateRange=pastWeek&type=all&storyText=false&prefix=false&page=0'
+
+  await page.goto(
+    'https://hn.algolia.com/' + query
+  )
+
+  const scrapedData = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('div.item-title-and-infos'))
+      .map(link => ({
+        title: link.querySelector('h2').textContent,
+        link: link.querySelector('a').getAttribute('href')
+      })
+    )
+  )
+
+  await browser.close()
+  return scrapedData
+}
+
 router.get('/news', (req, res) => {
   const mediumArticles = new Promise((resolve, reject) => {
     scrapeMedium()
@@ -32,9 +54,17 @@ router.get('/news', (req, res) => {
       .catch(err => reject('Medium scrape failed'))
   })
 
-  Promise.all([mediumArticles])
+  const hackerNews = new Promise((resolve, reject) => {
+    scrapeHackerNews()
+      .then(data => {
+        resolve(data)
+      })
+      .catch(err => reject('Hacker News scrape failed'))
+  })
+
+  Promise.all([mediumArticles, hackerNews])
     .then(data => {
-      res.render('news', { data: { articles: data[0] } })
+      res.render('news', { data: { articles: data[0], news: data[1] } })
     })
     .catch(err => res.status(500).send(err))
 })
