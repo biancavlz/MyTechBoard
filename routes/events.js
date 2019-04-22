e65b6992091a9cc600d9844511e0c0d766c69688
Bianca <ing_biank@hotmail.com>
2019-04-22 17:44:08 +0200

    gets data from API

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const router = express.Router();
+const axios = require('axios')
+const router = express.Router()
+
+const upcomingEvents = () => {
+    const query = `?key=${process.env.MEETUP_SECRET_KEY}&sign=true&text=javascript`
+    const url = `https://api.meetup.com/find/upcoming_events` + query
+
+    return axios
+        .get(url)
+        .then(response => {
+            const { data } = response
+            return data
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
 
 router.get('/events', (req, res) => {
-  res.render('events')
-  
+    upcomingEvents()
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.error('erro'))
 })
 
-module.exports = router;
+module.exports = router
