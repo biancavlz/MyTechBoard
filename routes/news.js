4e60c6eced693acc148e75583f72b785c85a78ec
Bianca <ing_biank@hotmail.com>
2019-04-23 16:41:43 +0200

    Returns promise when updates DB

--- routes/news.js
+++ routes/news.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const { scrapeMedium, scrapeHackerNews } = require("./tools/index");
-const News = require("../models/News");
-const puppeteer = require("puppeteer");
+const express = require('express')
+const { scrapeMedium, scrapeHackerNews } = require('./tools/index')
+const News = require('../models/News')
+const puppeteer = require('puppeteer')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/news", (req, res) => {
+router.get('/news', (req, res) => {
     Promise.all([scrapeMedium(), scrapeHackerNews()])
         .then(providers => {
             return Promise.all(
                 providers.map(provider => {
                     return provider.map(news => {
-                        return News.findOneAndUpdate(news, news, { upsert: true });
-                    });
+                        return News.findOneAndUpdate(news, news, { upsert: true }).then(data => {})
+                    })
                 })
-            );
+            )
         })
         .then(() => {
-            return Promise.all([News.find({})]);
+            return Promise.all([News.find({})])
         })
         .then(data => {
-            res.render("news", { data: { articles: data[0], news: data[1] } });
+            res.render('news', { data: { articles: data[0], news: data[1] } })
         })
-        .catch(err => res.status(500).send(err));
-});
+        .catch(err => res.status(500).send(err))
+})
 
-module.exports = router;
+module.exports = router
