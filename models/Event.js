dddea27c1cec7528b8a2b1042125c5bf9805f3af
Bianca <ing_biank@hotmail.com>
2019-04-25 11:26:40 +0200

    Adds event model

---
+++ models/Event.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema(
+  {
+    name: String,
+    link: String,
+    local_date: String,
+    local_time: String,
+    city: String,
+    address: String,
+    company: String,
+    visibility: String
+  }
+)
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
